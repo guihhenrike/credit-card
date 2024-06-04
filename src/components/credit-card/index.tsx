df67e9cb51bfb702803e150c48de48f5c94df90f
Guihhenrike <guih524@gmail.com>
2024-06-04 17:11:53 -0300

    rotate card

--- src/components/credit-card/index.tsx
+++ src/components/credit-card/index.tsx
@@ -1,9 +1,40 @@
 import { View, Text } from "react-native";
+import Animated, { useAnimatedStyle, SharedValue, interpolate, withTiming } from "react-native-reanimated";
+
 import { styles } from "./styles";
 
-export function CreditCard(){
+type CreditCardProps = {
+    cardSide: SharedValue<number>
+}
+
+export enum CARD_SIDE {
+    front = 0,
+    back = 1
+}
+
+export function CreditCard({ cardSide }: CreditCardProps){
+    const frontAnimatedStyles = useAnimatedStyle(() => {
+        const rotateValue = interpolate(cardSide.value, [CARD_SIDE.front, CARD_SIDE.back],[0, 180])
+       
+        return{
+            transform: [
+                { rotateY: withTiming(`${rotateValue}deg`, {duration: 1000 }) }
+            ]
+        }
+    })
+
+    const backAnimatedStyles = useAnimatedStyle(() => {
+        const rotateValue = interpolate(cardSide.value, [CARD_SIDE.front, CARD_SIDE.back],[180, 360])
+       
+        return{
+            transform: [
+                { rotateY: withTiming(`${rotateValue}deg`, {duration: 1000 }) }
+            ]
+        }
+    })
+
     return <View>
-        <View style={[styles.card, styles.front]}>
+        <Animated.View style={[styles.card, styles.front, frontAnimatedStyles]}>
             <View style={styles.header} >
                 <View style={[styles.circle, styles.logo]} />
                 <Text>Meu Cartão</Text>
@@ -17,10 +48,9 @@ export function CreditCard(){
                     <View style={[styles.circle, styles.orange]} />
                 </View>
             </View>
+        </Animated.View>
 
-        </View>
-
-        <View style={[styles.card, styles.back]}>
+        <Animated.View style={[styles.card, styles.back, backAnimatedStyles]}>
             <View>
                 <Text style={styles.label}>Número do cartão</Text>
                 <Text style={styles.value}>1234 5678 9012 3456</Text>
@@ -37,8 +67,7 @@ export function CreditCard(){
                     <Text style={styles.value}>123</Text>
                 </View>
             </View>
-
-        </View>
+        </Animated.View>
 
     </View>
 }
